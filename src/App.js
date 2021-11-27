2b5aa29b7a87b823eb7087bae75d924b70774862
yashwanth-sridharan <90434197+yashwanth-sridharan@users.noreply.github.com>
2021-11-27 18:25:54 -0500

    Added Header and other empty components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
-const App = () =>{
-    return(
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
-}
+import { CssBaseline, Grid } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </> 
+  );
+};
 
 export default App;
